7c7fa853bbcfa1e77337809658420feb8787ee4d
Portablefire22 <barnowlsarecool@gmail.com>
2024-02-22 00:20:58 +0000

    Prevented data cut off

--- testSketch/testSketch.ino
+++ testSketch/testSketch.ino
@@ -3,7 +3,7 @@
 struct Packet {
     uint8_t PacketKind; // What type of packet it is
     uint8_t PacketId;
-    uint8_t *RawData[32];     // Raw bytes of data, 2 bytes taken for first two args, one byte for end of text
+    uint8_t *RawData[32];     // Raw bytes of data, 2 bytes taken for first two args, one byte for end of text, leaving 29 bytes for data per communication
   };
 
 class PacketHandler {
@@ -16,11 +16,15 @@ class PacketHandler {
     data_to_send[0] = packet.PacketKind;
     data_to_send[1] = packet.PacketId;
     for (int i=2; i < 32; i++) {
-      data_to_send[i] = packet.RawData[i];
+      data_to_send[i] = packet.RawData[i - 2];
     }
     data_to_send[31] = 0x0D;
 
     Serial.write(data_to_send, 32); // 32 seems to be the magic number for outputting it all in one go
+
+  }
+
+  void insert_data(Packet* packet) {
 
   }
 
@@ -47,8 +51,8 @@ void setup() {
   Serial.begin(9600);
   pinMode(13, OUTPUT);
   uint8_t data[30];
-  for (int i=0; i<=29; i++) {
-    data[i] = 0x0A;
+  for (int i=0; i<29; i++) {
+    data[i] = i;
   }
   pack = packet_handler->create_packet(4, 1, data);
 }
