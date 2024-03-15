1e6eed7efdabadbd86e55f69ef2a92e414b194a4
Portablefire22 <barnowlsarecool@gmail.com>
2024-03-15 08:46:20 +0000

    Quick checklist

---
+++ Checklist.md
@@ -0,0 +1,30 @@
+TESTED - Has been tested and is known to work in current state
+UNTESTED - Has been implemented but not tested, functionality unknown
+PARTIALLY TESTED - The functionality was tested and worked but has since been modified in a way that affect results
+NO - Functionality just has not been implemented at all
+PARTIALLY WORKS - Has been tested, provides wanted results most of the time but is known to produce inaccurate or wrong outcomes
+
+# Crane
+## Functionality
+- Control Direction (TESTED)
+- Control Speed (UNTESTED)
+- Count revolutions (TESTED)
+- Monitor position of load (NO)
+- Strain-gauge (TESTED)
+- Bluetooth sender (PARTIALLY TESTED)
+## Data
+- Speed - Functionality is theoretically there but is missing data required for it to work
+- RPM - Should be functional (UNTESTED)
+- Revolutions completed - Functional (TEST)
+- Newtons (PARTIALLY WORKS)
+
+# Receiver
+## Functionality 
+- Receive data (PARTIALLY TESTED)
+- Send data to GUI (PARTIALLY TESTED)
+
+# GUI
+## Display
+- Integer (TESTED)
+- Float (TESTED)
+- String (TESTED)
--- final_arduino/crane/crane.ino
+++ final_arduino/crane/crane.ino
@@ -9,6 +9,8 @@
 #define SW2 12
 #define SW3 13
 
+#define RADIUS 2
+
 #define OPTICAL_PIN 2
 
 #define MOTOR_IN1 3
@@ -23,17 +25,20 @@
 
 //Create the Service and give at name and a uid
 BLEService AinService("4315b8fb-7cca-4ba6-a4c0-c3c0c915180f"); //This is a custom 128 bit service.It must be unique code, 
-//and new codes can be obtained from
-//https://www.guidgenerator.com/online-guid-generator.aspx
-//Add the characteristic - the example is a floating point number - give it a name
+
+// Characterisics aka setting up data transfer
+// All of the following have the following options: Write, notify changes, read, and broadcast 
 BLEIntCharacteristic Rev("4315b8fb-7cca-4ba6-a4c0-c3c0c915180f", BLEWrite | BLENotify | BLERead | BLEBroadcast); 
 BLEFloatCharacteristic Newton("c3ccbb8e-930c-4add-b57a-ce692b0c36ae", BLEWrite | BLENotify| BLERead | BLEBroadcast);
-//we can write, notify changes, read and broadcast the characteristic
+BLEFloatCharacteristic Speed("a3f5051e-d355-4dc6-bcb2-ed79a68d15a5", BLEWrite | BLENotify| BLERead | BLEBroadcast);
+BLEFloatCharacteristic Rpm("d1e76dde-62ea-4073-ab8a-005ff0536f63", BLEWrite | BLENotify| BLERead | BLEBroadcast);
 
-uint16_t oldRev = 0;  // last V reading from analog input
+uint16_t oldRev = 0;  // last reading of revolutions 
 
-int potenitometerValue = 0;
+int load_cell_in = 0; 
 float result = 0;
+
+uint8_t motor_speed_out = 127;
 
 //
 // BLE
@@ -89,9 +94,13 @@ void setup() {
   BLE.setAdvertisedService(AinService); //matches earlier
   AinService.addCharacteristic(Rev); // this is 19B10001-E8F2-537E-4F6C-D104768A1214
   AinService.addCharacteristic(Newton);
+  AinService.addCharacteristic(Rpm);
+  AinService.addCharacteristic(Speed);
   BLE.addService(AinService); // Add the Ain service to BLE
   Rev.writeValue(oldRev); // set initial value for this characteristic. We can write to the characteristic or read from it.
-
+  Newton.writeValue(0.0);
+  Rpm.writeValue(0.0);
+  Speed.writeValue(0.0);
   // start advertising BLE
   BLE.advertise();
   Serial.println("BLE peripheral advertising Ainmonitor...");
@@ -104,14 +113,17 @@ void setup() {
 
 }
 
-void calc_revs_per_minute {
+void calc_revs_per_minute() {
   uint16_t tmp = millis();
   tmp = (tmp / 1000)/60; // Convert to minutes to make it easier
-  rpm = (rev / 2.0) / tmp; 
+  rpm = (currentRev / 2.0) / tmp;
+  Rpm.writeValue(rpm);
 }
 
 float calc_speed() {
-  return (2 * PI * RADIUS) / rpm; 
+  float tmp = (2 * PI * RADIUS) / rpm;
+  Speed.writeValue(tmp);
+  return tmp; 
 }
 
 void opticalInterrupt() {
@@ -176,7 +188,6 @@ void displayHandler() {
   }
   else if (isClock) {
     display.println("Clockwise");
-
     digitalWrite(8, HIGH);
   } else if (!isClock) {
     display.println("Anti-CW");
@@ -198,7 +209,7 @@ void displayHandler() {
 
 void setClockwise() {
   isStopped = false;
-  analogWrite(MOTOR_EN, 127);
+  analogWrite(MOTOR_EN, motor_speed_out);
   digitalWrite(MOTOR_IN1, LOW);
   digitalWrite(MOTOR_IN2, HIGH);
   isClock = true;
@@ -208,7 +219,7 @@ void setClockwise() {
 // IN1: HIGH IN2: LOW
 void setAntiClockwise() {
   isStopped = false;
-  analogWrite(MOTOR_EN, 127);
+  analogWrite(MOTOR_EN, motor_speed_out);
   digitalWrite(MOTOR_IN2, LOW);
   digitalWrite(MOTOR_IN1, HIGH);
   isClock = false;
@@ -218,13 +229,29 @@ void setAntiClockwise() {
 void stopMotor() {
   digitalWrite(MOTOR_IN1, LOW);
   digitalWrite(MOTOR_IN2, LOW);
+  analogWrite(MOTOR_EN, LOW);
   isStopped = true;
 }
 
+// Really not sure how to even implement this considering we are already using all three buttons
+// I imagine it'd either be a button combination, reading the built-in potentiometer, or just pressing the button multiple times
+// I really don't know which is best
+void adjust_speed(boolean is_increase, uint8_t increment) {
+  if (is_increase && (int) motor_speed_out + (int) increment <= 255) { // Overflowing probably isn't a good idea
+    motor_speed_out += increment;
+  } else if (!is_increase && (int) motor_speed_out - (int) increment >= 0){ // Maybe we shouldn't underflow it either
+    motor_speed_out -= increment;
+  } else if (is_increase && (int) motor_speed_out + (int) increment > 255) { // Clamp it
+    motor_speed_out = 255;
+  } else if (!is_increase && (int) motor_speed_out - (int) < 0) {
+    motor_speed_out = 0;
+  }
+}
+
 void readValue() {
-  potenitometerValue = analogRead(LOAD_CELL_PIN_IN);
-  result = potenitometerValue * (3.3 / 1024.0);
-  float magic_number = 39.24 / 2.0;
+  load_cell_in = analogRead(LOAD_CELL_PIN_IN);
+  result = load_cell_in * (3.3 / 1024.0);
+  float magic_number = 39.24 / 2.0; // Theoretically the amount of Newtons per input voltage
   result = result * magic_number;
   Serial.println(result);
   Newton.writeValue(result);
